c8c67f131acee6d3de1386bb6775a4896b55ac63
Schleimfresse <87274892+Schleimfresse@users.noreply.github.com>
2023-01-21 14:52:05 +0100

    update 2.0

--- dynamic/server.js
+++ dynamic/server.js
@@ -2,30 +2,52 @@
 import http from "http";
 import path from "path";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import url from "url";
+import ejs from "ejs";
+import Datastore from "nedb";
+import crypto from "crypto";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const port = process.env.PORT;
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 if (process.env.NODE_ENV === "development") {
 }
 if (process.env.NODE_ENV === "production") {
 }
-import ejs from "ejs";
+const Database = new Datastore({ filename: "data.db", autoload: true });
 // Init - end -
 // routes
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     if (parsedUrl.pathname === "/") {
-        // Read the start page file
-        ejs.renderFile(__dirname + "/public/views/index.ejs", { data: { content: "TEXT" } }, (err, data) => {
-            if (err) {
+        Database.find({}, (err, docs) => {
+            ejs.renderFile(__dirname + "/public/views/index.ejs", { data: { content: JSON.stringify(docs) } }, (err, data) => {
+                if (err) {
+                    res.end(err);
+                }
+                else {
+                    res.end(data);
+                }
+            });
+        });
+    }
+    if (parsedUrl.pathname === "/add") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        req.on("end", () => {
+            try {
+                req.body = JSON.parse(body);
+                console.log(req.body);
+            }
+            catch (err) {
                 console.log(err);
-                res.end(err);
             }
-            else {
-                res.end(data);
-            }
+            Database.insert({ data: req.body.text, id: crypto.randomUUID(), date: new Date().getTime() }, () => { });
         });
     }
 });
--- dynamic/server.ts
+++ dynamic/server.ts
@@ -4,16 +4,22 @@ import http from "http";
 import path from "path";
 import dotenv from "dotenv";
 import fs from "fs";
+import bodyParser from "body-parser";
 import url from "url";
+import ejs from "ejs";
+import Datastore from "nedb";
+import crypto from "crypto";
 const __filename: string = url.fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
 dotenv.config();
 const port: number = process.env.PORT;
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 if (process.env.NODE_ENV === "development") {
 }
 if (process.env.NODE_ENV === "production") {
 }
-import ejs from "ejs";
+const Database = new Datastore({ filename: "data.db", autoload: true });
 // Init - end -
 
 // routes
@@ -22,13 +28,34 @@ const server = http.createServer((req, res) => {
 	const parsedUrl = url.parse(req.url, true);
 
 	if (parsedUrl.pathname === "/") {
-		// Read the start page file
-		ejs.renderFile(__dirname + "/public/views/index.ejs", { data: { content: "TEXT" } }, (err: Error, data: object) => {
-			if (err) {
-				res.end(err);
-			} else {
-				res.end(data);
+		Database.find({}, (err, docs) => {
+			ejs.renderFile(
+				__dirname + "/public/views/index.ejs",
+				{ data: { content: JSON.stringify(docs) } },
+				(err: Error, data: object) => {
+					if (err) {
+						res.end(err);
+					} else {
+						res.end(data);
+					}
+				}
+			);
+		});
+	}
+
+	if (parsedUrl.pathname === "/add") {
+		let body = "";
+		req.on("data", (chunk) => {
+			body += chunk.toString();
+		});
+		req.on("end", () => {
+			try {
+				req.body = JSON.parse(body);
+				console.log(req.body);
+			} catch (err) {
+				console.log(err);
 			}
+			Database.insert({ data: req.body.text, id: crypto.randomUUID(), date: new Date().getTime() }, () => {});
 		});
 	}
 });
